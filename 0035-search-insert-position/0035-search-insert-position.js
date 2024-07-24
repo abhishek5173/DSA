e67d8e3033aba06882f532eaf936cdf97e432452
Abhishek <114069034+abhishek5173@users.noreply.github.com>
2024-07-24 09:45:24 +0530

    Time: 57 ms (29.99%), Space: 49 MB (49.79%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.js
+++ 0035-search-insert-position/0035-search-insert-position.js
@@ -4,19 +4,18 @@
  * @return {number}
  */
 var searchInsert = function(nums, target) {
-   let result = 0;
-   for(let i=nums.length-1; i>=0; i--)
-   {
-        if(target>nums[i])
-        {
-            result = i+1;
-            break;
-        }
-        if(target==nums[i])
-        {
-            result = i;
-            break;
-        }
+   let low =0;
+   let high = nums.length-1;
+   let ans = nums.length;
+   while(low<=high){
+    let mid = Math.floor((low+high)/2);
+    if(nums[mid] >= target){
+        ans = mid;
+        high =mid-1;
+    }
+    else{
+        low = mid+1;
+    }
    }
-   return result; 
+   return ans;
 };
