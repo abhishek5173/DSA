0d56a5b14f2b26be4f3b6de803f75488c1b1b7dd
Abhishek <114069034+abhishek5173@users.noreply.github.com>
2024-07-26 11:41:23 +0530

    Time: 54 ms (44.41%), Space: 49.1 MB (22.97%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.js
+++ 0162-find-peak-element/0162-find-peak-element.js
@@ -3,16 +3,22 @@
  * @return {number}
  */
 var findPeakElement = function(nums) {
-    for (let i=0; i<nums.length; i++){
-        if((nums[i]> nums[i-1] && nums[i]> nums[i+1]) || (nums[i]> nums[i-1] || nums[i]> nums[i+1])){
-            return i;
+    if (nums.length ==1) return 0;
+    if(nums[0]>nums[1]) return 0;
+    if(nums[nums.length-1] > nums[nums.length-2]) return nums.length-1;
+    let low =1;
+    let high = nums.length-2;
+    while(low<=high){
+        let mid = Math.floor((low+high)/2);
+        if((nums[mid]>nums[mid-1]) && (nums[mid]>nums[mid+1])){
+            return mid;
         }
-        else if(nums.length ==2){
-            if(nums[i]>nums[i-1]){
-                return i;
-            }
+        else if(nums[mid]>nums[mid-1]){
+            low = mid+1
         }
-    }
-    
-    return 0;
+        else{
+            high = mid-1;
+        }
+    } 
+    return -1;
 };
