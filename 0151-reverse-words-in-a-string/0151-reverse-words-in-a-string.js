2407e314a111817ccc579818676555607a3c57d6
Abhishek <114069034+abhishek5173@users.noreply.github.com>
2024-08-01 22:45:03 -0700

    Time: 40 ms (99%), Space: 50.7 MB (67.67%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.js
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.js
@@ -3,26 +3,35 @@
  * @return {string}
  */
 var reverseWords = function(s) {
-   let n = s.length;
-   let ans="";
-   let i=0;
-   while(i < n){
-    let temp="";
-    while(s[i] == ' ' && i < n){
-        i++;
-    }
-    while(s[i] != ' ' && i < n ){
-        temp+=s[i]
-        i++;
-    }
-    if(temp.length>0){
-        if(ans.length==0){
-            ans=temp;
-        }
-        else{
-            ans = temp+" "+ans;
-        }
+   const str = s.split(' ');
+   const word = [];
+   for(let i = 0; i < str.length ; i++){
+    if(str[i] != ''){
+        word.push(str[i])
     }
    }
-   return ans;
+   return word.reverse().join(' ');
 };
+
+// let n = s.length;
+//    let ans="";
+//    let i=0;
+//    while(i < n){
+//     let temp="";
+//     while(s[i] == ' ' && i < n){
+//         i++;
+//     }
+//     while(s[i] != ' ' && i < n ){
+//         temp+=s[i]
+//         i++;
+//     }
+//     if(temp.length>0){
+//         if(ans.length==0){
+//             ans=temp;
+//         }
+//         else{
+//             ans = temp+" "+ans;
+//         }
+//     }
+//    }
+//    return ans;
