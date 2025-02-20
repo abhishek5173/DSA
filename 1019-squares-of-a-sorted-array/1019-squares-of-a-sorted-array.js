3f691466abf459f6ba55a072805b3bf053e3f111
Abhishek <114069034+abhishek5173@users.noreply.github.com>
2025-02-20 22:43:02 +0530

    Time: 4 ms (75.89%), Space: 61.7 MB (5.74%) - LeetHub

--- 1019-squares-of-a-sorted-array/1019-squares-of-a-sorted-array.js
+++ 1019-squares-of-a-sorted-array/1019-squares-of-a-sorted-array.js
@@ -3,9 +3,23 @@
  * @return {number[]}
  */
 var sortedSquares = function(nums) {
-    for(let i=0; i< nums.length; i++){
-        nums[i] = nums[i]**2;
+    let left = 0
+    let right = nums.length -1;
+    let index = nums.length -1;
+    let result = new Array(nums.length).fill(0)
+
+    while(left <= right){
+        let leftsq = nums[left]**2;
+        let rightsq = nums[right]**2;
+        
+        if(leftsq > rightsq){
+            result[index] = leftsq
+            left++;
+        }else{
+            result[index]= rightsq
+            right--;
+        }
+        index--
     }
-    nums.sort((a,b) => a-b)
-    return nums
+    return result
 };
