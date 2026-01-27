a9dd090990fb0ab398a2c226afbfef89b725b3fe
Abhishek <114069034+abhishek5173@users.noreply.github.com>
2026-01-27 21:06:15 +0530

    Revisit Remove element

--- 0027-remove-element/0027-remove-element.js
+++ 0027-remove-element/0027-remove-element.js
@@ -6,7 +6,7 @@
 var removeElement = function(nums, val) {
   for(let i=nums.length -1; i >=0;i--){
     if(nums[i] === val){
-        nums.splice(i,1)
+        nums.splice(i,1) 
     }
   }
   return nums.length
